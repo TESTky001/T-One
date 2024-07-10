0591911e22b87c18b333937db05dd5b24686ca9e
kycheng <kycheng@tencent.com>
2024-07-10 14:47:04 +0800

    add passwd test

--- s1.go
+++ s1.go
@@ -2,7 +2,7 @@ func SelectHandler3(db *sql.DB) func(w http.ResponseWriter, req *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		del := req.URL.Query().Get("del")
 		id := req.URL.Query().Get("Id")
-		example_12345678901234567890
+		const passwd = "h90000UJUJPp9349FEfexx8LcCOuCPOY"
 		if del == "del" {
 			sql := "SELECT * FROM table WHERE Id = "
 			// ruleid: tainted-sql-string
